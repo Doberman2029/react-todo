5349170003e30afcf87e5ac1c234066fbb5cdfb3
Doberman2029 <yakushina20@mail.ru>
2020-10-28 16:24:59 +0300

    Updates

---
+++ precache-manifest.a88a49d3af46dd430f0511c8fc387bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f198ac9a6fecb455770fb0a1607287",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "ca33cb7232380a7d6f78",
+    "url": "/react-todo/static/css/main.c774e71c.chunk.css"
+  },
+  {
+    "revision": "1662988d5781a13eeb89",
+    "url": "/react-todo/static/js/2.3619738e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo/static/js/2.3619738e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca33cb7232380a7d6f78",
+    "url": "/react-todo/static/js/main.c2a47b36.chunk.js"
+  },
+  {
+    "revision": "8b60092a6ae08cc228b0",
+    "url": "/react-todo/static/js/runtime-main.40cbbb06.js"
+  }
+]);
